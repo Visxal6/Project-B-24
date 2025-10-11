e522c9c5eddf973ea86051db30c1b0a73eea919a
Jonathan Orellana <jonathanorellana@Jonathans-MacBook-Pro-2.local>
2025-10-11 16:27:09 -0400

    create templates for views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Render html for a request path
+def home(request):
+    return render(request, 'app/home.html')
