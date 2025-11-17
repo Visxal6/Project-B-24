ae25bf061d748e6524a0c8bf770f6ddb14c2763c
pbp5hy <pbp5hy@virginia.edu>
2025-11-16 21:15:43 -0500

    initial userprofile edits to setup

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class UserRegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=150, required=True)
@@ -21,3 +22,8 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email", "username"]
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ["bio", "sustainability_interests"]
