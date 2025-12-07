e57857c3f22d219c9d41207c67352ec1867860ba
Visxal <119636260+Visxal6@users.noreply.github.com>
2025-12-06 20:32:29 -0500

    Remove empty line in migration file

--- users/migrations/0004_notification.py
+++ users/migrations/0004_notification.py
@@ -4,7 +4,6 @@ from django.conf import settings
 
 class Migration(migrations.Migration):
 
-
     dependencies = [
         ('users', '0003_merge_0002_profile_display_name_0002_profilepicture'),
     ]
